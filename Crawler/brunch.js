6488f7ced4c418fe4622ed8983237401691683bb
k906506 <k906506@naver.com>
2021-06-19 22:47:13 +0900

    무한 스크롤 구현(not api)

--- Crawler/brunch.js
+++ Crawler/brunch.js
@@ -9,9 +9,22 @@ const puppeteer = require('puppeteer');
       width:1440,
       height:900,
   })
+
   await page.goto('https://brunch.co.kr/search');
   await page.click("input.txt_search"); // 검색창 클랙
   await page.keyboard.type("Hello World") // 검색어 입력
   await page.keyboard.press("Enter");
+  await page.waitForNavigation(); // 일시적 대기
+
+  let infinteScrollInterval = setInterval(async () => {
+      await page.evaluate(() => {
+          window.scrollBy(0, window.innerHeight);
+      });
+    }, 1000); // 1000 ms마다 실행
+
+
+    setTimeout(async () => {
+        clearInterval(infinteScrollInterval);
+    }, 1000 * 10); // 10000 ms가 지나면 종료
   // await browser.close();
 })();
