2f4c22b0cd6dadb29aef3e365f02d5eb47d32da8
k906506 <k906506@naver.com>
2021-06-21 01:12:57 +0900

    오류 수정

--- Crawler/brunchData.js
+++ Crawler/brunchData.js
@@ -2,10 +2,9 @@ const axios = require("axios"); // axios 호출
 const cheerio = require('cheerio'); // cheerio 호출
 
 let article = {};
-let pageNumber = 0;
 const crawler = (pageNumber) => {
     axios
-        .get(`https://api.brunch.co.kr/v1/search/article?q=hello&page${pageNumber}=&pageSize=20&highlighter=y&escape=y&sortBy=accu`
+        .get(`https://api.brunch.co.kr/v1/search/article?q=Hello%20World&page${pageNumber}=&pageSize=20&highlighter=y&escape=y&sortBy=accu`
         )
         .then((response) => {
             const data = response.data;
@@ -19,3 +18,5 @@ const crawler = (pageNumber) => {
             console.log(article);
         });
 };
+
+crawler(1);
